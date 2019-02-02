0dd85ff51df80d08701f3bb8b4963277c9d17a72
wenyao02.li <wenyao02.li@vipshop.com>
2019-02-02 15:10:51 +0800

    修改子项目版本

--- liwy-commons-lang/src/main/java/com/liwy/commons/lang/json/JsonUtils.java
+++ liwy-commons-lang/src/main/java/com/liwy/commons/lang/json/JsonUtils.java
@@ -1,9 +1,11 @@
 package com.liwy.commons.lang.json;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+
 /**
- * <b>模块：</b> <br/>
- * <b>名称：</b> <br/>
- * <b>描述：</b> <br/>
+ * <b>名称：</b> JSON工具类<br/>
  * <b>作者：</b> wenyao02.li <br/>
  * <b>创建时间：</b> 2018/6/7 16:41 <br/>
  * <b>版本：</b> V1.0 <br/>
@@ -21,6 +23,7 @@ public class JsonUtils {
      * 实体类对象转换为JSON字符串
      */
     public static String toJsonStr(Object obj) {
+        JSON.toJSONString(new ArrayList<>());
         return null;
     }
 
--- liwy-commons-lang/src/test/java/com/liwy/commons/lang/math/MathUtilsTest.java
+++ liwy-commons-lang/src/test/java/com/liwy/commons/lang/math/MathUtilsTest.java
@@ -1,5 +1,14 @@
 package com.liwy.commons.lang.math;
 
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.function.DoubleToIntFunction;
+
 /**
  * <b>模块：</b> <br/>
  * <b>名称：</b> <br/>
@@ -9,4 +18,90 @@ package com.liwy.commons.lang.math;
  * <b>版本：</b> V1.0 <br/>
  */
 public class MathUtilsTest {
+
+    /**
+     * <b>描述：</b> 测试浮点类型数据是否相等<br/>
+     * <b>作者：</b>wenyao02.li<br/>
+     * <b>版本：</b>V1.0 <br/>
+     *
+     * @param
+     * @return void
+     */
+    @Test
+    public void testFloatEqual() {
+        double dNumber = 3.146d;
+        float myNumber = 3.146f;
+        double d = myNumber;
+        System.out.println(dNumber);
+        System.out.println(myNumber);
+        System.out.println(d);
+        System.out.println(Double.toString(dNumber));
+        System.out.println(Float.toString(myNumber));
+        System.out.println(Double.toString(d));
+        Assert.assertFalse(myNumber==dNumber);
+    }
+
+    @Test
+    public void test() {
+        System.out.println(0.05);
+        System.out.println(0.01);
+        System.out.println(1.0);
+        System.out.println(0.42);
+        System.out.println(4.015);
+        System.out.println(123.3);
+        System.out.println(Double.toString(0.05));
+        System.out.println(Double.toString(0.01));
+        System.out.println(Double.toString(1.0));
+        System.out.println(Double.toString(0.42));
+        System.out.println(Double.toString(4.015));
+        System.out.println(Double.toString(123.3));
+        double a = 0.05;
+        double b = 0.01;
+        double c = 1.0;
+        double d = 0.42;
+        double e = 4.015;
+        double f = 123.3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(Double.toString(a));
+        System.out.println(Double.toString(b));
+        System.out.println(Double.toString(c));
+        System.out.println(Double.toString(d));
+        System.out.println(Double.toString(e));
+        System.out.println(Double.toString(f));
+
+        System.out.println(0.05+0.01);
+        System.out.println(1.0-0.42);
+        System.out.println(4.015*100);
+        System.out.println(123.3/100);
+        System.out.println(new BigDecimal(Double.toString(4.015)).multiply(new BigDecimal(Double.toString(100))));
+    }
+
+    @Test
+    public void test1() {
+        double d = 301353.05123456789123456789;
+        System.out.println(d);
+        System.out.println(Double.toString(d));
+
+        BigDecimal decimal = new BigDecimal(d);
+        System.out.println(decimal);//301353.0499999999883584678173065185546875
+        System.out.println(decimal.setScale(1, RoundingMode.HALF_UP));//301353.0
+
+        System.out.println(new BigDecimal(Double.toString(d)));
+        System.out.println(new BigDecimal(new Double(d).toString()));
+        System.out.println(new BigDecimal("301353.05"));
+        System.out.println(new BigDecimal("301353.895898895455898954895989"));
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(Float.floatToIntBits(0.1f));
+        System.out.println(Integer.toBinaryString(Float.floatToIntBits(0.1f)));
+        System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.1f)));
+        System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.1d)));
+    }
 }
